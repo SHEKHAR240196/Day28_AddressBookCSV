41226d4a4167414adbdb91d27ff798fd7b22f35a
shekhar <shekharpatle999@gmail.com>
2021-05-12 18:09:29 +0530

    Added: AddressBook CSV and JSON File Operation

---
+++ src/main/java/com/bridgelabz/ScannerForAddressBook.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+import java.util.Scanner;
+public class ScannerForAddressBook {
+    public Scanner scannerProvider() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner;
+    }
+}
